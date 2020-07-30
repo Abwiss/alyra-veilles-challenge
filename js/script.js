2a5c3110de401a2b48e095256babcc192a226268
Abwiss <fchbrt1@gmail.com>
2020-07-30 12:30:49 +0200

    start 1.3

--- js/script.js
+++ js/script.js
@@ -1,32 +1,31 @@
 "use strict"
 
 const dateNow = moment();
-// on met la langue en français
 dateNow.locale("fr");
-
-/*
-// on affiche la date d'aujourd'hui en plusieurs formats
-console.log("dddd", dateNow.format("dddd"));
-console.log("YYYY", dateNow.format("YYYY"));
-console.log("YY", dateNow.format("YY"));
-console.log("MMMM", dateNow.format("MMMM"));
-console.log("dddd D MMMM YYYY", dateNow.format("dddd D MMMM YYYY"));
-console.log("dddd DD/MM/YYYY", dateNow.format("dddd DD/MM/YYYY"));
-console.log("dddd DD/MM/YY", dateNow.format("dddd DD/MM/YY"));
-
-// Choisis le format le plus adapté et ajoute un paragraphe au début de la page, juste après la titre h1, le contenu du paragrahe devrait être : "Nous sommes ...." avec la date du jour.
- 
-const h1 = document.querySelector("h1");
-
-const dateP = document.createElement("p");
-
-dateP.textContent = `Bonjour nous sommes le ${dateNow.format("dddd D MMMM YYYY")}`;
-
-h1.after(dateP);
-*/
-
 const h1 = document.getElementById("h1")
 console.log(h1)
-
 let introP = `<p class="bg-primary text-center text-light py-3">Bonjour nous sommes le ${dateNow.format("dddd D MMMM YYYY")}</p>`
 h1.insertAdjacentHTML("afterend", introP)
+
+function insertVeilles() {
+    const listeVeilles = document.getElementById('liste-veilles')
+    const ulEl = document.createElement('ul')
+    ulEl.classList.add('row', 'list-unstyled')
+    console.log(ulEl)
+    for (let el of entries) {
+        console.log(el)
+        const li = document.createElement('li')
+     //   li.classList.add()
+     li.innerHTML = `<div>
+     <h2 class="card">${el.subject}</h2>
+     <p>${el.category}</p>
+     <p>${el.date}</p>
+    
+     </div>`
+     ulEl.append(li)
+    }
+   listeVeilles.prepend(ulEl)
+}
+
+insertVeilles()
+
